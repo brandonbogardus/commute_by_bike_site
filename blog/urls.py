195a516a282ac36674d3dded5cd1eb86c5ace65f
Brandon Bogardus <brandonbogardus@gmail.com>
2026-04-01 18:57:06 -0700

    Changed blog model attributes.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 app_name = "blog"
 
 url_patterns = [
     # blog index
-    path("", BlogListView.as_view(), name="index")
+    path("", BlogListView.as_view(), name="index"),
+    path("<int:pk>/", BlogDetailView.as_view(), name="detial"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 
 class BlogListView(ListView):
+    model = Post
+    ordering = ["-date_posted"]
+    paginate_by = 5
+    template_name = "blog/index.html"
+    content_type = "posts"
+
+
+class BlogDetailView(DetailView):
     model = Post
