743bc01444c2c14ef4aad811e8fe11c820e071ce
Brandon Bogardus <brandonbogardus@gmail.com>
2026-04-07 17:39:57 -0700

    Added users app and templates.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy("blog:index")
+
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+
