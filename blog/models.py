7a648ffd0a01f103bf4315d29d0e5a4e6f1bdabc
Brandon Bogardus <brandonbogardus@gmail.com>
2026-04-14 17:41:25 -0700

    Added PostImage model.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
-from django.conf import settings
 from django.db import models
+from django.conf import settings
 from django.urls import reverse
 
 
@@ -9,10 +9,21 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(
+        "users.CustomUser", on_delete=models.CASCADE, null=True, blank=True
+    )
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("blog:detail", kwargs={"pk": self.pk})
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="post_images/")
+    caption = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Image for: {self.post.title}"
