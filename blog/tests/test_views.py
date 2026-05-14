3d5cf6834f8364f2b404dc1af88fd13e9063d4bc
Brandon Bogardus <brandonbogardus@gmail.com>
2026-05-13 17:54:16 -0700

    Added more tests.

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,8 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from blog.models import Post
 
+
+User = get_user_model()
 
 class BlogListViewTests(TestCase):
-    pass
+
+    def test_blog_list_view(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_list_view_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "blog/post_list.html")
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username="testuser",
+            password="password123",
+        )
+
+        cls.post = Post.objects.create(
+            title="Test Post",
+            content="Some content.",
+            author=cls.user,
+        )
+
+    def test_blog_list_view_contains_post(self):
+        response = self.client.get("/")
+        self.assertIn(self.post, response.context["object_list"])
+        
+
 
 
 class BlogDetailViewTests(TestCase):
