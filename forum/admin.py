6f1bb55747fddcb0c9db1bf91087c6715cc2d336
Brandon Bogardus <brandonbogardus@gmail.com>
2026-06-14 16:42:46 -0700

    Added forum tempaltes.

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Thread, Comment
 
-# Register your models here.
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "date_posted"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "author", "thread", "date_posted"]
