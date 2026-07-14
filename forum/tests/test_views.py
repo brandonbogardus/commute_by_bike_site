d4932fc23b7e00889ae851d2cdb95431390d6675
Brandon Bogardus <brandonbogardus@gmail.com>
2026-07-13 17:24:26 -0700

    Added more forum tests.

--- forum/tests/test_views.py
+++ forum/tests/test_views.py
@@ -28,3 +28,25 @@ class ForumListViewTests(TestCase):
     def test_forum_list_view_contains_thread(self):
         response = self.client.get("/forum/")
         self.assertIn(self.thread, response.context["object_list"])
+
+
+class ForumDetailViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username="testuser",
+            password="password123",
+        )
+        cls.thread = Thread.objects.create(
+            title="Test Thread.",
+            content="Some content.",
+            author=cls.user,
+        )
+
+    def test_forum_detail_view(self):
+        response = self.client.get(f"/forum/{self.thread.pk}/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_forum_detail_view_template(self):
+        response = self.client.get(f"/forum/{self.thread.pk}/")
+        self.assertTemplateUsed(response, "forum/thread_detail.html")
